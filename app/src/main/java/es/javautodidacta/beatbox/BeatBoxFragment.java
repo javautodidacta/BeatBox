5ececa54afa54597652c61882ff8cafd847bf471
Miguel Callejón Berenguer <mcberenguer@gmail.com>
2018-01-28 20:53:02 +0100

    BeatBoxFragment created and hosting activity.

---
+++ app/src/main/java/es/javautodidacta/beatbox/BeatBoxFragment.java
@@ -0,0 +1,16 @@
+package es.javautodidacta.beatbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * This class ...
+ *
+ * @author Miguel Callejón Berenguer
+ * @version 2018.01
+ */
+
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+}
