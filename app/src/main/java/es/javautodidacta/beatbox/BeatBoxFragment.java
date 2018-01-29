a099b1be9f00e1fa95ab8efbb94569148b56e1ce
Miguel Callejón Berenguer <mcberenguer@gmail.com>
2018-01-29 23:00:05 +0100

    Observable data implemented.

--- app/src/main/java/es/javautodidacta/beatbox/BeatBoxFragment.java
+++ app/src/main/java/es/javautodidacta/beatbox/BeatBoxFragment.java
@@ -58,6 +58,12 @@ public class BeatBoxFragment extends Fragment {
         private SoundHolder(ListItemSoundBinding binding) {
             super(binding.getRoot());
             mBinding = binding;
+            mBinding.setViewModel(new SoundViewModel(mBeatBox));
+        }
+
+        public void bind(Sound sound) {
+            mBinding.getViewModel().setSound(sound);
+            mBinding.executePendingBindings();
         }
     }
 
@@ -79,7 +85,8 @@ public class BeatBoxFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(SoundHolder holder, int position) {
-
+            Sound sound = mSounds.get(position);
+            holder.bind(sound);
         }
 
         @Override
---
+++ app/src/main/java/es/javautodidacta/beatbox/SoundViewModel.java
@@ -0,0 +1,35 @@
+package es.javautodidacta.beatbox;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+/**
+ * This class is the View Model between
+ * the View ({@code list_item_model.xml}) and the Model ({@link Sound}.
+ *
+ * @author Miguel Callejón Berenguer
+ * @version 2018.01
+ */
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+}
